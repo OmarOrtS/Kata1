86a23a60cf1c8ac11125368d3c1aa65a918838f2
Test <omar.ortega102@alu.ulpgc.es>
2024-09-29 19:42:31 +0100

    kata1 project finished

--- src/software/ulpgc/es/Main.java
+++ src/software/ulpgc/es/Main.java
@@ -1,7 +1,9 @@
 package software.ulpgc.es;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Person());
+        System.out.println(new Person("Pepe", LocalDate.of(2000, 10, 23)));
     }
 }
